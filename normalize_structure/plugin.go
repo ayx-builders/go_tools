b2b35f6317c2e441b69acb3db64ebed43a2cc9ce
tlarsen <larsenthomasj@gmail.com>
2022-04-04 09:22:05 -0400

    add shell for normalize structure

--- normalize_structure/plugin.go
+++ normalize_structure/plugin.go
@@ -1,1 +1,29 @@
 package normalize_structure
+
+import (
+	"github.com/tlarsen7572/goalteryx/sdk"
+)
+
+type NormalizeStructure struct {
+	provider sdk.Provider
+	output   sdk.OutputAnchor
+	outInfo  *sdk.OutgoingRecordInfo
+}
+
+func (n *NormalizeStructure) Init(provider sdk.Provider) {
+	n.provider = provider
+	n.output = provider.GetOutputAnchor(`Output`)
+}
+
+func (n *NormalizeStructure) OnInputConnectionOpened(connection sdk.InputConnection) {
+	inInfo := connection.Metadata()
+	n.outInfo = inInfo.Clone().GenerateOutgoingRecordInfo()
+	n.output.Open(n.outInfo)
+}
+
+func (n *NormalizeStructure) OnRecordPacket(connection sdk.InputConnection) {
+}
+
+func (n *NormalizeStructure) OnComplete() {
+	n.output.Close()
+}
--- normalize_structure/plugin_test.go
+++ normalize_structure/plugin_test.go
@@ -1,1 +1,19 @@
 package normalize_structure
+
+import (
+	"github.com/tlarsen7572/goalteryx/sdk"
+	"testing"
+)
+
+func TestNormalizeStructure(t *testing.T) {
+	plugin := &NormalizeStructure{}
+	runner := sdk.RegisterToolTest(plugin, 1, `<Configuration><StringType>V_WString</StringType><StringLength>200</StringLength><IntType>Int64</IntType><FloatType>Double</FloatType></Configuration>`)
+	collector := runner.CaptureOutgoingAnchor(`Output`)
+	runner.ConnectInput(`Input`, `test.txt`)
+
+	runner.SimulateLifecycle()
+
+	if fieldLen := len(collector.Data); fieldLen != 11 {
+		t.Fatalf(`expected 11 fields but got %v`, fieldLen)
+	}
+}
